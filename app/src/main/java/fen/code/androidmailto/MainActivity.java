8c2d73b54e04e0ac282884b5f3ec5349786c50c4
Fenton Martin <fen.official@gmail.com>
2017-06-13 16:57:50 +0700

    Add Declaration on Main

--- app/src/main/java/fen/code/androidmailto/MainActivity.java
+++ app/src/main/java/fen/code/androidmailto/MainActivity.java
@@ -1,13 +1,31 @@
 package fen.code.androidmailto;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonSend;
+    EditText editEmail, editSubject, editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSend = (Button) findViewById(R.id.button_send);
+        editEmail = (EditText) findViewById(R.id.edit_email);
+        editSubject = (EditText) findViewById(R.id.edit_subject);
+        editText = (EditText) findViewById(R.id.edit_text);
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
